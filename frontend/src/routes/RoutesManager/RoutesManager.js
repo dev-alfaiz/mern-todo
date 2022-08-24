b5e4fe79459fbd595c492c3e5961d4e1a80351e8
dev-alfaiz <captainalfaiz@gmail.com>
2022-08-24 18:00:41 +0530

    frontend/implemented private component

--- frontend/src/routes/RoutesManager/RoutesManager.js
+++ frontend/src/routes/RoutesManager/RoutesManager.js
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { Routes, Route } from "react-router-dom";
 
+import { PrivateComponent } from "../../components";
+
 import {
   AddPage,
   Home,
@@ -14,17 +16,24 @@ import {
 export const RoutesManager = () => {
   return (
     <Routes>
-      <Route exact path={"/"} element={<Home />} key={"default-page"} />
-      <Route exact path={"/home"} element={<Home />} key={"home-page"} />
-      <Route exact path={"/todos"} element={<Home />} key={"todos"} />
-      <Route exact path={"/add-todo"} element={<AddPage />} key={"add-todo"} />
-      <Route
-        exact
-        path={"/update-todo"}
-        element={<UpdatePage />}
-        key={"update-todo"}
-      />
-      <Route exact path={"/profile"} element={<Profile />} key={"profile"} />
+      <Route element={<PrivateComponent />}>
+        <Route exact path={"/"} element={<Home />} key={"default-page"} />
+        <Route exact path={"/home"} element={<Home />} key={"home-page"} />
+        <Route exact path={"/todos"} element={<Home />} key={"todos"} />
+        <Route
+          exact
+          path={"/add-todo"}
+          element={<AddPage />}
+          key={"add-todo"}
+        />
+        <Route
+          exact
+          path={"/update-todo"}
+          element={<UpdatePage />}
+          key={"update-todo"}
+        />
+        <Route exact path={"/profile"} element={<Profile />} key={"profile"} />
+      </Route>
       <Route exact path={"/login"} element={<Login />} key={"login"} />
       <Route exact path={"/signup"} element={<SignUp />} key={"sign-up"} />
       <Route
