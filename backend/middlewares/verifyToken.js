8ff3c9d34aa961a37acc786de2f3c7cecf4138c7
dev-alfaiz <captainalfaiz@gmail.com>
2022-08-24 12:36:40 +0530

    backend/implemented register api

---
+++ backend/middlewares/verifyToken.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+const jwtKey = "mern-todo-2022";
+
+const verifyToken = (request, response, next) => {
+  let token = request.headers["authorization"];
+  if (token) {
+    token = token.split(" ")[1];
+    jwt.verify(token, jwtKey, (error, valid) => {
+      if (error) {
+        response.status(401).send({ result: "Please provide a valid token" });
+      } else {
+        next();
+        s;
+      }
+    });
+  } else {
+    response.status(403).send({ result: "Please add token with headers." });
+  }
+};
+
+module.exports = verifyToken;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,45 @@
+// Packages Imports
+const express = require("express");
 const app = require("./app");
 const dotenv = require("dotenv");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
 
-dotenv.config({path:"./.env"})
+require("./database/config");
+const jwtKey = "mern-todo-2022";
+const User = require("./database/User");
+const verifyToken = require("./middlewares/verifyToken");
+
+// ENV Config
+dotenv.config({ path: "./.env" });
+
+// App Use
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.post("/register", async (request, response) => {
+  try {
+    if (Object.keys(request.body).length) {
+      let user = new User(request.body);
+      let result = await user.save();
+      result = result.toObject();
+      delete result.password;
+      jwt.sign({ result }, jwtKey, { expiresIn: "2h" }, (error, token) => {
+        if (error) {
+          response.send({
+            result: "Something went wrong, please try after some time.",
+          });
+        }
+        response.send({ result: result, auth: token });
+      });
+    } else {
+      response.status(400).send({ result: "Register details must" });
+    }
+  } catch (error) {
+    response.status(400).send({ result: error });
+  }
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
