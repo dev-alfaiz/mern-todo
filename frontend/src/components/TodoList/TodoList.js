b8d747922f7a154f3e2fe526088a9310726e9701
dev-alfaiz <captainalfaiz@gmail.com>
2022-08-28 18:55:59 +0530

    frontend/implemented the timestamp in todo list with moment

--- frontend/src/components/TodoList/TodoList.js
+++ frontend/src/components/TodoList/TodoList.js
@@ -1,12 +1,14 @@
 import * as React from "react";
 import "./TodoList.css";
 import { Link } from "react-router-dom";
+import moment from "moment";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Paper from "@mui/material/Paper";
 import { useDispatch } from "react-redux";
@@ -30,6 +32,7 @@ export const TodoList = ({ data }) => {
               <TableCell>#</TableCell>
               <TableCell align="right">Title</TableCell>
               <TableCell align="right">Body</TableCell>
+              <TableCell align="right">Date</TableCell>
               <TableCell align="right">Operations</TableCell>
             </TableRow>
           </TableHead>
@@ -48,6 +51,27 @@ export const TodoList = ({ data }) => {
                     {todo.body.length > 19
                       ? todo.body.slice(0, 20) + "..."
                       : todo.body}
+                  </TableCell>
+                  <TableCell align="right">
+                    {todo.createdAt ? (
+                      <>
+                        <Typography>
+                          {moment(todo.createdAt).format("MMMM Do YYYY")}
+                        </Typography>
+                        <Typography>
+                          On: {moment(todo.createdAt).format("h:mm:ss a")}
+                        </Typography>
+                      </>
+                    ) : (
+                      <>
+                        <Typography>
+                          {moment(todo.updatedAt).format("MMMM Do YYYY")}
+                        </Typography>
+                        <Typography>
+                          On: {moment(todo.updatedAt).format("h:mm:ss a")}
+                        </Typography>
+                      </>
+                    )}
                   </TableCell>
                   <TableCell
                     align="right"
--- frontend/src/screens/Home/Home.js
+++ frontend/src/screens/Home/Home.js
@@ -1,31 +1,17 @@
 import * as React from "react";
-// import moment from "moment";
 import "./Home.css";
 import { useDispatch, useSelector } from "react-redux";
 
 import { getAllTodos } from "../../app/slices/todoSlice";
 import { TodoList } from "../../components";
 
-const STATIC_TODO = [
-  {
-    _id: "63075e90ecc0c77e19802b45",
-    title: "Kabaddi at 12:00 PM",
-    body: "Play with team at kshirsagar ground.",
-    userId: "63075e6decc0c77e19802b43",
-    __v: 0,
-  },
-];
-
 export const Home = () => {
   const dispatch = useDispatch();
   const todos = useSelector((state) => state.todoReducer.todos);
-  // const auth = localStorage.getItem("user");
-  // const authDetail = JSON.parse(auth);
 
   React.useEffect(() => {
     dispatch(getAllTodos());
   }, []);
-
 
   return (
     <div className="home-page">
